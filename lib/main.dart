3a35fd9b161511500cc8d80bc762acd02532c962
Mert Demir <Mert.Demir@hensoldt-analytics.com>
2021-11-05 17:11:47 +0300

    flutter_native_splash added & modified for testing

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: const Center(
-          child: Text('Body Text')
+          child: Text('Text Text')
       ),
     );
   }
